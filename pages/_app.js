3af82e6a2eac60188fb115262cb7a0e874cbe3a9
SelimSevgi <slmsvg44@gmail.com>
2021-12-17 17:41:16 +0300

    :pencil2: yemeksepeti homepage olusturuldu header ve content tamamlandi

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/Header.css";
+import "../styles/Content.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-top">
+        <div className="header-logo">
+          <img src="https://assets.yemeksepeti.com/images/ys-new-logo.svg"></img>
+        </div>
+        <div className="header-english-select">
+          <a href="#"> ENGLISH</a>
+        </div>
+      </div>
+    </div>
+  );
+}
